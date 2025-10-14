4c9fe09ebde0dcb5c60b3d2c2e96fad7c6f8ce2b
Mathias Mohs <mathias.mohs@web.de>
2025-10-14 20:12:13 +0200

    Test zurück

--- print('Testing if __file__ is defined wh.py
+++ print('Testing if __file__ is defined wh.py
@@ -1,1 +1,10 @@
-print('Testing if __file__ is defined when executing a script with -c option')
+import matplotlib.pyplot as plt
+
+
+
+x=[i for i in range(10) if i%2==0]
+y=[i**2 for i in x]
+plt.plot(x,y)
+plt.show()
+
+
